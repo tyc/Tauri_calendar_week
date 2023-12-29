1db4d6198a57f9066044d820f96bf980774760e1
Tehn Yit Chin <tehn.yit.chin@gmail.com>
2023-12-29 21:46:02 +0100

    added the two functions but not really working.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,30 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn startup(name: &str) -> String {
+  println!("executing handle_button1");
+  format!("Hello, {}!", name)
+}
+
+
+#[tauri::command]
+fn handle_left_button(name: &str) -> String {
+  println!("executing handle_button1");
+  format!("Hello, {}!", name)
+}
+
+
+#[tauri::command]
+fn handle_right_button(name: &str) -> String {
+  println!("exec    uting handle_button2");
+  format!("Hello, {}!", name)
+}
+
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![startup, handle_left_button, handle_right_button])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
-}
+  }
